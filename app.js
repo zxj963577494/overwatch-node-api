2cec77ae9271ac3eba99f5235c7a3b20e2d5c54f
Zheng XuJiang <963577494@qq.com>
2018-10-18 20:30:19 +0800

    fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 module.exports = app => {
   app.beforeStart(async () => {
-    await app.runSchedule('get_owl_maps')
+    // await app.runSchedule('get_owl_maps')
     // await app.runSchedule('get_owl_rankings')
     // await app.runSchedule('get_owl_teams')
   })
